6943195d79dfad4796da33d76cd2436d0c53af90
ASalem404 <ahmed.salem.prime@gmail.com>
2024-10-16 05:01:29 +0300

    🌐 Add rout table and route table association resource

--- main.tf
+++ main.tf
@@ -28,3 +28,23 @@ resource "aws_internet_gateway" "asa_internet_gateway" {
     Stage = "dev"
     }
 }
+
+resource "aws_route_table" "asa_public_rt" {
+  vpc_id = aws_vpc.asa_vpc.id
+
+  tags = {
+    Name = "asa_route_table"
+    Stage = "dev"
+    }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.asa_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.asa_internet_gateway.id
+}
+
+resource "aws_route_table_association" "asa_public_subnet_association" {
+  subnet_id = aws_subnet.asa_public_subnet.id
+  route_table_id = aws_route_table.asa_public_rt.id
+}
