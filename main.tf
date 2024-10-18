dd390706705808d88921e5e32d7e5df3937f1ee1
ASalem404 <ahmed.salem.prime@gmail.com>
2024-10-18 23:09:26 +0300

    👔 add providers and config file

--- main.tf
+++ main.tf
@@ -1,61 +1,61 @@
 resource "aws_vpc" "asa_vpc" {
-  cidr_block = "10.123.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-    Name = "asa_vpc"
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name  = "asa_vpc"
     Stage = "dev"
-    }
+  }
 }
 
 
 resource "aws_subnet" "asa_public_subnet" {
-  vpc_id = aws_vpc.asa_vpc.id
-  cidr_block = "10.123.1.0/24"
-    availability_zone = "us-east-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.asa_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
-    Name = "asa_public_subnet"
+  tags = {
+    Name  = "asa_public_subnet"
     Stage = "dev"
-    }
+  }
 }
 
 resource "aws_internet_gateway" "asa_internet_gateway" {
   vpc_id = aws_vpc.asa_vpc.id
-    tags = {
-    Name = "asa_internet_gateway"
+  tags = {
+    Name  = "asa_internet_gateway"
     Stage = "dev"
-    }
+  }
 }
 
 resource "aws_route_table" "asa_public_rt" {
   vpc_id = aws_vpc.asa_vpc.id
 
   tags = {
-    Name = "asa_route_table"
+    Name  = "asa_route_table"
     Stage = "dev"
-    }
+  }
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.asa_public_rt.id
+  route_table_id         = aws_route_table.asa_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.asa_internet_gateway.id
+  gateway_id             = aws_internet_gateway.asa_internet_gateway.id
 }
 
 resource "aws_route_table_association" "asa_public_subnet_association" {
-  subnet_id = aws_subnet.asa_public_subnet.id
+  subnet_id      = aws_subnet.asa_public_subnet.id
   route_table_id = aws_route_table.asa_public_rt.id
 }
 
 
 resource "aws_security_group" "asa_sg" {
-    vpc_id = aws_vpc.asa_vpc.id
-    name = "asa_sg"
-    description = "Allow inbound traffic on port 22"
+  vpc_id      = aws_vpc.asa_vpc.id
+  name        = "asa_sg"
+  description = "Allow inbound traffic on port 22"
 
-ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -69,10 +69,10 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-      tags = {
-        Name = "asa_sg"
-        Stage = "dev"
-    }
+  tags = {
+    Name  = "asa_sg"
+    Stage = "dev"
+  }
 }
 
 resource "aws_key_pair" "asa_auth" {
@@ -82,18 +82,31 @@ resource "aws_key_pair" "asa_auth" {
 
 
 resource "aws_instance" "dev_node" {
-  ami = data.aws_ami.server_ami.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.asa_auth.key_name
+  ami             = data.aws_ami.server_ami.id
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.asa_auth.key_name
   security_groups = [aws_security_group.asa_sg.name]
-  subnet_id = aws_subnet.asa_public_subnet.id
-  user_data = file("userdata.sh")
+  subnet_id       = aws_subnet.asa_public_subnet.id
+  user_data       = file("userdata.sh")
 
   root_block_device {
     volume_size = 8
   }
+
+  provisioner "local-exec" {
+    command = templatefile("windows-ssh-config.sh", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/asakey"
+
+    })
+    interpreter = ["powershell", "-Command"]
+  }
+
+
+
   tags = {
-    Name = "dev_node"
+    Name  = "dev_node"
     Stage = "dev"
   }
 
